c47550c7c5a23f41f22ed323b7123f204da58cec
Zainan Victor Zhou <zzn@zzn.im>
2012-07-18 14:15:14 -0700

    added Iso2HelloWorld

---
+++ kickoff/Ios2HelloWorld/Ios2HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  Ios2HelloWorld
+//
+//  Created by Victor Zhou on 7/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
---
+++ kickoff/Ios2HelloWorld/Ios2HelloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ios2HelloWorld
+//
+//  Created by Victor Zhou on 7/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HelloWorldAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HelloWorldAppDelegate class]));
+    }
+}
